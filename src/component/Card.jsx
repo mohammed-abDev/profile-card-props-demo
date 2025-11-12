26eac95a9834850e6c849357ffae162d90014365
mohammed-abDev <mohammedabdudev11@gmail.com>
2025-11-12 17:07:51 +0300

    add Card.jsx-component

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -4,7 +4,14 @@ import "../App.css"
 function Card(props) {
   return (
     < >
-         
+          <div className="profile-card">
+            <img src={props.img} alt="Hile" className="avatar" />
+            <div className="info">
+              <h2>{props.name}</h2>
+              <p className="age">{props.age}</p>
+              <p className="role">{props.role}</p>
+            </div>
+          </div>
     </>
       
   );
