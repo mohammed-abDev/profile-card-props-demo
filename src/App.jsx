f61fa83138d126d17fe9b72ddd2209b24a508b99
mohammed-abDev <mohammedabdudev11@gmail.com>
2025-11-12 17:02:36 +0300

    add all react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import chaltuImg from "./assets/img/chaltu.avif";
+import haileImg from "./assets/img/haile.avif"
+import julitImg from "./assets/img/julit.avif";
+import Card from "./component/Card";
+
+function App() {
+
+  return (
+    
+  );
+}
+
+export default App
